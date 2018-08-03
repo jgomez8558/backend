1bd1bbd074b539271889f40cdc187383d052c766
Jon Gomez <jgomez8558@gmail.com>
2018-08-03 16:37:37 -0500

    Adding Table to DB

---
+++ models/users.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var users = sequelize.define('users', {
+    UserId: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    FirstName: DataTypes.STRING,
+    LastName: DataTypes.STRING,
+    Adress: DataTypes.STRING,
+    Phone: DataTypes.INTEGER,
+    Role: DataTypes.INTEGER,
+    Email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    Username: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    Password: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  }, {});
+  users.associate = function(models) {
+    // associations can be defined here
+  };
+  return users;
+};
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+// Connecting to DB imports
+const sqlite = require('sqlite3').verbose();
+// Auth Models
+var models = require('../models');
+const auth = require("../config/auth");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
